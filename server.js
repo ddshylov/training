90703c1a929e26a0cee115b0c44c2d96342c8c4e
ddshylov <zegzal704341@gmail.com>
2024-09-06 12:52:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!</h1>');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+console.log(`hello, im here`);
